0565e688de3d9ecdf554a4190f8ec1ee871a5e85
Denis Efremov <efremov.a.denis@gmail.com>
2017-03-14 04:30:21 +0300

    Cache $this var

--- resources/js/search.js
+++ resources/js/search.js
@@ -1,7 +1,9 @@
 $(function() {
     $('select.select2').each(function() {
-        if (!$(element).hasClass('select2-hidden-accessible')) {
-            $(this).select2({
+        var $this = $(this);
+        
+        if (!$this.hasClass('select2-hidden-accessible')) {
+            $this.select2({
                 containerCssClass: 'form-control custom-select select2-override',
                 dropdownCssClass: 'select2-option-override',
             });
