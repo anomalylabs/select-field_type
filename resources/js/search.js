ff7cdced7b95df61078534cabbdc65ba44de606d
Denis Efremov <efremov.a.denis@gmail.com>
2017-03-14 03:32:30 +0300

    Checked - works

--- resources/js/search.js
+++ resources/js/search.js
@@ -1,9 +1,8 @@
 $(function() {
-    $(".select2").each(function(i, element) {
-        if (!$(element).hasClass('select2-hidden-accessible')) 
-            $(element).select2({
-                containerCssClass: "form-control custom-select select2-override",
-                dropdownCssClass: "select2-option-override",
-            });
+    $('select.select2').each(function() {
+        $(this).select2({
+            containerCssClass: 'form-control custom-select select2-override',
+            dropdownCssClass: 'select2-option-override',
+        });
     });
 });
