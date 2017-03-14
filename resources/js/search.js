2d66a25e2392e155cfca8bebe29ab0012feb4fb3
Denis Efremov <efremov.a.denis@gmail.com>
2017-03-14 04:29:12 +0300

    Update search.js

--- resources/js/search.js
+++ resources/js/search.js
@@ -1,8 +1,10 @@
 $(function() {
     $('select.select2').each(function() {
-        $(this).select2({
-            containerCssClass: 'form-control custom-select select2-override',
-            dropdownCssClass: 'select2-option-override',
-        });
+        if (!$(element).hasClass('select2-hidden-accessible')) {
+            $(this).select2({
+                containerCssClass: 'form-control custom-select select2-override',
+                dropdownCssClass: 'select2-option-override',
+            });
+        }
     });
 });
